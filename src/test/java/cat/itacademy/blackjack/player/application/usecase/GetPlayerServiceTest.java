574d4d394a9098f5d350669c6f72aa8d893b15da
PauG <paugaston@hotmail.com>
2026-01-24 17:16:30 +0100

    refactor: removing unnecessary imports

--- src/test/java/cat/itacademy/blackjack/player/application/usecase/GetPlayerServiceTest.java
+++ src/test/java/cat/itacademy/blackjack/player/application/usecase/GetPlayerServiceTest.java
@@ -6,7 +6,6 @@ import cat.itacademy.blackjack.player.domain.port.in.PlayerResponse;
 import cat.itacademy.blackjack.player.domain.port.out.PlayerRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -14,8 +13,6 @@ import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
