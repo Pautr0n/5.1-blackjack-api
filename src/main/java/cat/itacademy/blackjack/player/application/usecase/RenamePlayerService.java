7c799b0e48b6fa4e5aa67e60314dd408b1751fad
PauG <paugaston@hotmail.com>
2026-01-24 17:15:58 +0100

    feat: RenamePlayerService implemented

---
+++ src/main/java/cat/itacademy/blackjack/player/application/usecase/RenamePlayerService.java
@@ -0,0 +1,36 @@
+package cat.itacademy.blackjack.player.application.usecase;
+
+import cat.itacademy.blackjack.player.domain.model.Player;
+import cat.itacademy.blackjack.player.domain.model.PlayerId;
+import cat.itacademy.blackjack.player.domain.model.exception.PlayerNotFoundException;
+import cat.itacademy.blackjack.player.domain.port.in.PlayerResponse;
+import cat.itacademy.blackjack.player.domain.port.in.RenamePlayerUseCase;
+import cat.itacademy.blackjack.player.domain.port.out.PlayerRepository;
+import reactor.core.publisher.Mono;
+
+public class RenamePlayerService implements RenamePlayerUseCase {
+
+    private final PlayerRepository playerRepository;
+
+    public RenamePlayerService(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    @Override
+    public Mono<PlayerResponse> rename(String id, String newName) {
+        PlayerId playerId = new PlayerId(id);
+
+        return playerRepository.findById(playerId)
+                .switchIfEmpty(Mono.error(new PlayerNotFoundException("Player not found")))
+                .flatMap(player -> {
+                    Player renamed = player.rename(newName);
+                    return playerRepository.save(renamed);
+                })
+                .map(saved -> new PlayerResponse(
+                        saved.id().value(),
+                        saved.name(),
+                        saved.score()
+                ));
+    }
+
+}
