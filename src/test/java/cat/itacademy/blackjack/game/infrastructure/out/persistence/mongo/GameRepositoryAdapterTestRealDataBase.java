2861ab93066ab6313bfe924143d7ff8653ce4c08
PauG <paugaston@hotmail.com>
2026-01-25 18:40:13 +0100

    test: testing GameRepositoryAdapter with real database successfully

---
+++ src/test/java/cat/itacademy/blackjack/game/infrastructure/out/persistence/mongo/GameRepositoryAdapterTestRealDataBase.java
@@ -0,0 +1,35 @@
+package cat.itacademy.blackjack.game.infrastructure.out.persistence.mongo;
+
+import cat.itacademy.blackjack.game.domain.model.Deck;
+import cat.itacademy.blackjack.game.domain.model.Game;
+import cat.itacademy.blackjack.game.domain.model.GameId;
+import cat.itacademy.blackjack.game.domain.port.out.GameRepository;
+import cat.itacademy.blackjack.player.domain.model.PlayerId;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.test.StepVerifier;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class GameRepositoryAdapterTestRealDataBase {
+
+    @Autowired
+    private GameRepository repository;
+
+    @Test
+    void save_and_find_game() {
+        Game game = Game.start(GameId.newId(),"123", Deck.standard52Cards());
+
+        StepVerifier.create(repository.save(game))
+                .assertNext(saved -> assertThat(saved.id()).isEqualTo(game.id()))
+                .verifyComplete();
+
+        StepVerifier.create(repository.findById(game.id()))
+                .assertNext(found -> assertThat(found.id()).isEqualTo(game.id()))
+                .verifyComplete();
+    }
+
+
+}
